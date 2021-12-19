98e9083960cfa43ea78ae8345581c16a751a5c1c
Muhammad Ahmed <ahmed495802@gmail.com>
2021-12-19 21:36:26 +0500

    added sample footer

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -28,6 +28,9 @@
       integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
       crossorigin="anonymous"
     ></script>
+
+    <script src="https://kit.fontawesome.com/bcccf98175.js" crossorigin="anonymous"></script>
+    
 </head>
 <body>
     {{View::make('header')}}
