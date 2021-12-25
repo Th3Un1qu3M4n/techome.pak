d918147494e6a69c36d65b7d939cd75c5963b8b3
Muhammad Ahmed <ahmed495802@gmail.com>
2021-12-25 16:27:17 +0500

    Updated: Styled category Table

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,33 +1,41 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="card">
+    <div class="card mb-5">
         <div class="card-header">
             <div class="row">
-                <div class="col-md-9 col-sm-7">
-                    <h3>Category Page</h3>
+                <div class="col-sm-10 col-7">
+                    <h2>Category Page</h2>
                 </div>
-                <div class="col-md-3 col-sm-5">
-                    <a class="btn bg-gradient-dark mb-0" href="{{url('/add-category')}}"><i class="material-icons text-sm">add</i>&nbsp;&nbsp; New Category</a>
+                <div class="col-sm-2 col-5">
+                    <a class="btn bg-gradient-dark mb-0" href="{{url('/add-category')}}"><i class="material-icons text-sm">add</i>&nbsp;&nbsp; New</a>
                 </div>
             </div>
         </div>
-        <div class="card-body">
+        
+    </div>
+    <div class="card">
+        <div class="card-header p-0 position-relative mt-n4 mx-5 z-index-2">
+            <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
+              <h6 class="text-white ps-3">Categories Table</h6>
+            </div>
+        </div>
+        <div class="card-body px-0 pb-2">
             <div class="table-responsive p-0">
                 <table class="table align-items-center mb-0">
-                        <thead class="bg-gradient-primary pt-4 pb-3">
-                            <tr class="text-white">
-                                <th>Id</th>
-                                <th>Name</th>
-                                <th>Description</th>
-                                <th>Image</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
+                    <thead class="pt-4 pb-3">
+                        <tr class="text-center opacity-8">
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Image</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
                     <tbody>
                         @foreach ($categories as $category)
-                            <tr>
-                                <td>{{$category->id}}</td>
+                            <tr class="text-center">
+                                <td >{{$category->id}}</td>
                                 <td>{{$category->name}}</td>
                                 <td>{{$category->description}}</td>
                                 <td><img src="{{asset('assets/uploads/category/'.$category->image)}}" alt="category img" style="height: 150px; width:150px;"></td>
