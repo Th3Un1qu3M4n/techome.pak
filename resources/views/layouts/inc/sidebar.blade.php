66011c6fdb0743d7d57fb9a7d2191212c8207764
Muhammad Ahmed <ahmed495802@gmail.com>
2022-01-10 19:30:36 +0500

    bug fixed: admin navbar home

--- resources/views/layouts/inc/sidebar.blade.php
+++ resources/views/layouts/inc/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0" href="/dashboard" target="_blank">
+      <a class="navbar-brand m-0" href="/" target="_blank">
         
         <h2 class="ms-1 font-weight-bold text-white">Techome</h2>
       </a>
